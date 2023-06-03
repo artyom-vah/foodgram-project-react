5bbf9b4ea75644e81476b902035c0d2b1db3ff76
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-06-03 09:21:42 +0500

    Исправление замечаний после 2го ревью-6

--- backend/api/management/commands/load_data.py
+++ backend/api/management/commands/load_data.py
@@ -1,8 +1,7 @@
 import json
 
-from django.core.management.base import BaseCommand
-
 from api.models import Ingredient, Tag
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
--- backend/api/models.py
+++ backend/api/models.py
@@ -3,7 +3,6 @@ from django.core.validators import (MaxValueValidator, MinValueValidator,
                                     RegexValidator)
 from django.db import models
 from django.db.models import UniqueConstraint
-
 from users.models import User
 
 LENGTH_OF_RECIPES = 200
